0123efb1ef864a7d9faae3e7d63dacee2fc4a153
harshsoni13 <harshsoni7121@gmail.com>
2026-06-13 18:54:30 +0530

    stock

---
+++ Array/StockBuy121.java
@@ -0,0 +1,11 @@
+public class StockBuy121 {
+    public int maxProfit(int[] prices) {
+        int minPrice=Integer.MAX_VALUE;
+        int profit=0;
+        for(int price:prices){
+            minPrice=Math.min(minPrice,price);
+            profit=Math.max(profit,price-minPrice);
+        }
+        return profit;
+    }
+}  
