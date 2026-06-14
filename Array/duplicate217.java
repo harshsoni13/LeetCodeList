498ae0d4365c54f41af5f54b51eb87cde834aa76
harshsoni13 <harshsoni7121@gmail.com>
2026-06-14 23:56:09 +0530

    duplicate

---
+++ Array/duplicate217.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class duplicate217 {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set=new HashSet<>();
+        for(int num:nums){
+            if(set.contains(num)){
+                return true;
+            }
+            else{
+                set.add(num);
+            }
+        }
+        return false;
+        
+    }
+}
