c529dc0e042e15191c28c87ccb8481009cc4459e
harshsoni13 <harshsoni7121@gmail.com>
2026-06-15 18:36:42 +0530

    kadanesAlgo

---
+++ Array/Maximum53.java
@@ -0,0 +1,17 @@
+public class Maximum53 {
+    public int maxSubArray(int[] nums) {
+       int n=nums.length;
+       int sum=0;
+       int maxi=nums[0];
+       for(int i=0;i<n;i++){
+        sum+=nums[i];
+        maxi=Math.max(maxi,sum);
+        if(sum<0){
+            sum=0;
+        }
+       }
+       return maxi;
+    }
+}
+    
+
