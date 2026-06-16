01dd651e932f307eacf0d6f7faf8b735abdf850c
harshsoni13 <harshsoni7121@gmail.com>
2026-06-16 12:01:12 +0530

    Create MovesZero283.java

---
+++ Array/MovesZero283.java
@@ -0,0 +1,18 @@
+public class MovesZero283 {
+    public void moveZeroes(int[] nums) {
+        int n=nums.length;
+        int i=0;
+        for(int j=0;j<n;j++){
+            if(nums[j]!=0){
+                nums[i]=nums[j];
+                i++;
+            }
+        }
+        for(int k=i;k<n;k++){
+            nums[k]=0;
+        }
+        
+    }
+}
+    
+
