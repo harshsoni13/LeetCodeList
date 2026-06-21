1bfa81302930929e7e42c19b9a9be4fdc62c5210
harshsoni13 <harshsoni7121@gmail.com>
2026-06-21 22:49:55 +0530

    Create SecondLarElement.java

---
+++ Array/SecondLarElement.java
@@ -0,0 +1,16 @@
+
+    import java.util.*;
+class Solution {
+    public int getSecondLargest(int[] arr) {
+        int n=arr.length;
+     Arrays.sort(arr);
+     for(int i=n-2;i>=0;i--){
+         if(arr[i]!=arr[n-1]){
+             return arr[i];
+         }
+     }
+        return -1;
+    }
+}
+    
+
