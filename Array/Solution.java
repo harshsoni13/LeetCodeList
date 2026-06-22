5246752ec76cc260f3366b558820d049e2dcbc82
harshsoni13 <harshsoni7121@gmail.com>
2026-06-22 23:50:23 +0530

    leet 485

---
+++ Array/Solution.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int max=0;
+        int n=nums.length;
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==1){
+                count++;
+                max=Math.max(max,count);
+            }else{
+                count=0;
+            }
+        }
+        return max;
+    }
+} 
