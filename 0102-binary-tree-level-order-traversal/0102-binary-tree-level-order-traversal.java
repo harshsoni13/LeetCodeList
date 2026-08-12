75c65a2ee4be50b2734dc22a1dd6f0d1cf49c236
harshsoni13 <harshsoni7121@gmail.com>
2026-08-12 22:30:07 +0530

    Time: 1 ms (95.85%), Space: 46.7 MB (81.19%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -15,27 +15,25 @@
  */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> ans=new ArrayList<>();
-        Queue<TreeNode> q=new LinkedList();
-        if(root==null){
-            return ans;
+        List<List<Integer>> out=new ArrayList<>();
+       Queue<TreeNode> queue=new LinkedList<>();
+       if(root==null){
+        return out;
+       }
+       queue.add(root);
+       while(!queue.isEmpty()){
+        int size=queue.size();
+        List<Integer> level=new ArrayList<>();
+        for(int i=0;i<size;i++){
+            TreeNode node=queue.poll();
+            level.add(node.val);
+        
+        if(node.left!=null){
+            queue.add(node.left);
         }
-        q.add(root);
-        while(!q.isEmpty()){
-            int n=q.size();
-            List<Integer> li=new ArrayList<>();
-            for(int i=0;i<n;i++){
-                TreeNode node=q.poll();
-                li.add(node.val);
-                if(node.left!=null){
-                    q.add(node.left);
-                }
-                if(node.right!=null){
-                    q.add(node.right);
-                }
-            }
-            ans.add(li);
+        if(node.right!=null){
+            queue.add(node.right);
         }
-        return ans;
-    }
-}
+       }
+       out.add(level);}
+       return out;}}
