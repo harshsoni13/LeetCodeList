9b973be5f373db9e2005902f75ad6d0a16c1a2e3
harshsoni13 <harshsoni7121@gmail.com>
2026-09-17 19:15:14 +0530

    Time: 3 ms (99.81%), Space: 46.4 MB (42.72%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -4,6 +4,7 @@ class Solution {
         String[] symbols={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
         StringBuilder ans=new StringBuilder();
         for(int i=0;i<values.length;i++){
+          
             while(num>=values[i]){
                 ans.append(symbols[i]);
                 num-=values[i];
